bc063f6a62acc6ebb16be21f9e4dfd95ffbda59f
Andre Pires <brainshutdown@gmail.com>
2022-06-03 11:55:53 +0100

    Redux store setup + Navbar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import store and provider
+import { store } from './store';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
 	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
 );
